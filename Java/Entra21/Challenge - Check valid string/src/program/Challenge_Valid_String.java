c5df207b863feb88a6b764600fd6e8b4974fdb72
Moucalou <moucalou@gmail.com>
2025-04-08 14:31:35 -0300

    Update Challenge_Valid_String.java

--- Java/Entra21/Challenge - Check valid string/src/program/Challenge_Valid_String.java
+++ Java/Entra21/Challenge - Check valid string/src/program/Challenge_Valid_String.java
@@ -1,10 +1,32 @@
 package program;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Challenge_Valid_String {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		List<Character> list = new ArrayList<>();
+		String s = "({[]})";
+	
+		for (int i = 0; i < s.length(); i ++) {
+			if (s.charAt(i) == '(' || s.charAt(i) == '[' || s.charAt(i) == '{') {
+				list.add(s.charAt(i));
+			} else if (s.charAt(i) == ')' && list.getLast() == '(') {
+				list.removeLast();
+			} else if (s.charAt(i) == ']' && list.getLast() == '[') {
+				list.removeLast();
+			} else if (s.charAt(i) == '}' && list.getLast() == '{') {
+				list.removeLast();
+			}			
+		}
+		
+		if (list.size() == 0) {
+			System.out.println("True");
+		} else {
+			System.out.println("False");
+		}
 
 	}
-
 }
